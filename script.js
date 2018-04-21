8c4cc2f6526fe3f848a0122fc2d364941b061d7e
Martin <tinchetsu@gmail.com>
2018-04-21 01:31:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+function roll() {
+    var genresArray = [];
+    var rand1, rand2;
+    var desc1, desc2;
+    var indexDesc2
+    for (var key in genres.genres) {
+        genresArray.push({genre:key, desc:genres.genres[key]});
+    }
+    rand1 = Math.floor(Math.random() * genresArray.length);
+    do {
+        rand2 = Math.floor(Math.random() * genresArray.length);
+    } while (rand2==rand1);
+    
+    
+    document.getElementById("genre1").textContent = genresArray[rand1].genre;
+    document.getElementById("genre2").textContent = genresArray[rand2].genre;
+    
+    desc1 = genresArray[rand1].desc;
+    
+    desc1 = desc1.slice(0, desc1.length-1)
+    
+    if (genresArray[rand2].desc.toLowerCase().indexOf("games")>=0) {
+        indexDesc2 = genresArray[rand2].desc.toLowerCase().indexOf("games") + 6;
+        desc2 = genresArray[rand2].desc.substr(indexDesc2);
+    } else if (genresArray[rand2].desc.toLowerCase().indexOf("game")>=0) {
+        indexDesc2 = genresArray[rand2].desc.toLowerCase().indexOf("game") + 5;
+        desc2 = genresArray[rand2].desc.substr(indexDesc2);
+    } else {
+        desc2 = genresArray[rand2].desc;
+    }
+    
+    document.getElementById("desc1").textContent = desc1;
+    document.getElementById("desc2").textContent = desc2;
+    
+    
+    
+}
